b90392756d58bfe2703082ce5e825bb36d42f9a7
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-04-11 15:43:13 -0400

    add new conponent and style it

---
+++ src/components/RecipePreview.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './RecipePreview.scss'
+
+const RecipePreview = ({ recipe }) => {
+  console.log(recipe);
+  return (
+    <div className="preview">
+      <img src={recipe.image} alt="recipe.title" />
+      <h2>{recipe.dishTypes[0]}</h2>
+      <h3>{recipe.title}</h3>
+      <p className="author">By {recipe.sourceName}</p> 
+    </div>
+  );
+};
+
+export default RecipePreview;
