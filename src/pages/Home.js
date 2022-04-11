506277bf2ad7f8c5b15cfb409b6495ddf158657d
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-04-11 13:18:26 -0400

    add new pages

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../components/Banner'
+import Popular from '../components/Popular'
+
+const Home = () => {
+  return (
+    <div>
+      <Banner />
+      <Popular />
+    </div>
+  )
+}
+
+export default Home
