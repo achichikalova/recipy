458cc7c9a61402bd677ed4a743fb6f8db87d7eab
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-04-11 20:02:16 -0400

    refactore code, change styles

--- src/components/RecipePreview.js
+++ src/components/RecipePreview.js
@@ -1,14 +1,13 @@
 import React from "react";
-import './RecipePreview.scss'
+import "./RecipePreview.scss";
 
 const RecipePreview = ({ recipe }) => {
-  console.log(recipe);
   return (
     <div className="preview">
       <img src={recipe.image} alt="recipe.title" />
       <h2>{recipe.dishTypes[0]}</h2>
       <h3>{recipe.title}</h3>
-      <p className="author">By {recipe.sourceName}</p> 
+      <p className="author">By {recipe.sourceName}</p>
     </div>
   );
 };
