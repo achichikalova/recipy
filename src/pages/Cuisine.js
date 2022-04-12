ce74325b1a5c4d0e454aa7a957c8949e4d3bc1bf
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-04-12 11:41:44 -0400

    implement cuisine component

--- src/pages/Cuisine.js
+++ src/pages/Cuisine.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import RecipePreview from "../components/RecipePreview";
 
 const Cuisine = () => {
-  return (
-    <div>Cuisine</div>
-  )
-}
+  const [cuisines, setCuisines] = useState([]);
+  console.log(cuisines);
+  let params = useParams();
 
-export default Cuisine
+  useEffect(() => {
+    getCuisine(params.type);
+  }, [params.type]);
+
+  const getCuisine = async (type) => {
+    const URL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${type}`;
+
+    await axios
+      .get(URL)
+      .then((res) => {
+        setCuisines(res.data.results);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const cuisine = cuisines.map((cuisine) => {
+    return (
+      <div key={cuisine.id}>
+        <img src={cuisine.image} alt={cuisine.title} />
+        <h4>{cuisine.title}</h4>
+      </div>
+    );
+  });
+
+  return <div>{cuisine}</div>;
+};
+
+export default Cuisine;
