31f06dd3a6344a968d0cb49384fbefc5e4dbc2a9
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-04-11 20:00:36 -0400

    add new component and style it

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import './Banner.scss'
+const axios = require("axios");
 
 const Banner = () => {
-  return (
-    <div>Banner</div>
-  )
-}
+  const [recipe, setRecipe] = useState();
 
-export default Banner
+  useEffect(() => {
+    getRecipe();
+  }, []);
+
+  const getRecipe = async () => {
+    const URL = `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=1`;
+    await axios
+      .get(URL)
+      .then((res) => {
+        setRecipe(res.data.recipes[0]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="banner">
+      <img src={recipe.image} alt="recipe.title" />
+      <h2>{recipe.dishTypes[0]}</h2>
+      <h3>{recipe.title}</h3>
+      <p className="author">By {recipe.sourceName}</p>
+    </div>
+  );
+};
+
+export default Banner;
