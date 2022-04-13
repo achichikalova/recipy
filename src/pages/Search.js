8abc939b7e959db02070617d19803e8de152e4ac
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-04-13 14:10:10 -0400

    add Search page layout and style it

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Category from "../components/Category";
+import "./Search.scss";
+import { FaSearch } from "react-icons/fa";
+import { MdClear } from "react-icons/md";
 
 const Search = () => {
   return (
-    <div>Search</div>
-  )
-}
+    <div className="search">
+      <Category />
+      <form className="input-wrap">
+        <FaSearch className="search-icon" />
+        <input type="text" placeholder="Find a recipe..." />
+        <MdClear className="cancel-icon"/>
+      </form>
+    </div>
+  );
+};
 
-export default Search
+export default Search;
