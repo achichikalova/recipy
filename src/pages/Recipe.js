1d9787299683a3da95f256676753595cba2b7fc7
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-04-18 17:57:38 -0400

    implement fetching ingredients for recipe from API, add more details to the page

--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -8,43 +8,71 @@ import { WiTime5 } from "react-icons/wi";
 
 const Recipe = () => {
   const [recipeInfo, setRecipeInfo] = useState({});
+  const [ingredients, setIngredients] = useState([]);
 
   const params = useParams();
-
-  useEffect(() => {
-    getRecipeInfo(params.id);
-  }, []);
 
   const getRecipeInfo = async (id) => {
     const URL = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${process.env.REACT_APP_API_KEY}`;
     await axios
       .get(URL)
       .then((res) => {
+        console.log(res.data);
         setRecipeInfo(res.data);
+        setIngredients(res.data.extendedIngredients);
       })
       .catch((err) => {
-        console.error(err);
+        console.log(err);
       });
   };
 
+  useEffect(() => {
+    getRecipeInfo(params.id);
+  }, [params.id]);
+
+  const ingredient = ingredients.map((ingredient) => {
+    return <li key={ingredient.id}>{ingredient.original}</li>;
+  });
+
   return (
     <div className="recipe-info-container">
-      <h1>{recipeInfo.title}</h1>
+      <h1>{recipeInfo?.title}</h1>
       <div className="recipe-meta">
         <div className="meta">
           <AiFillLike />
-          <h3>{recipeInfo.aggregateLikes}</h3>
+          <h3>{recipeInfo?.aggregateLikes}</h3>
         </div>
         <div className="meta">
           <FaUserTimes />
-          <h3>{recipeInfo.servings}</h3>
+          <h3>{recipeInfo?.servings}</h3>
         </div>
         <div className="meta">
           <WiTime5 />
-          <h3>{recipeInfo.readyInMinutes} min</h3>
+          <h3>{recipeInfo?.readyInMinutes} min</h3>
         </div>
       </div>
-      <img src={recipeInfo.image} alt={recipeInfo.title} />
+      <div className="recipe-info">
+        <div className="image">
+          <img src={recipeInfo?.image} alt={recipeInfo?.title} />
+          <p>
+            {recipeInfo?.title} By {recipeInfo?.sourceName}
+          </p>
+        </div>
+        <div className="summary">
+          <button>Summary</button>
+          <p dangerouslySetInnerHTML={{ __html: recipeInfo.summary }}></p>
+        </div>
+        <div className="ingredients">
+          <button>Ingredients</button>
+          <ul>{ingredient}</ul>
+        </div>
+        <div className="instructions">
+          <button>Instructions</button>
+          {recipeInfo.instructions}
+        </div>
+
+        <div className="directions">{/* <ul>{directions}</ul> */}</div>
+      </div>
     </div>
   );
 };
