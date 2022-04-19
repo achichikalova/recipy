2aefd81e8cc1acb3d448244725db81b4fdb25543
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-04-19 09:34:10 -0400

    change layout for the component

--- src/components/RecipePreview.js
+++ src/components/RecipePreview.js
@@ -3,12 +3,11 @@ import { Link } from "react-router-dom";
 import "./RecipePreview.scss";
 
 const RecipePreview = ({ recipe }) => {
+  console.log(recipe);
   return (
     <Link to={`/recipe/${recipe.id}`} className="preview">
-      {recipe.image && <img src={recipe.image} alt="recipe.title" />}
-      {recipe.dishTypes && <h2>{recipe.dishTypes[0]}</h2>}
+      {recipe.image && <img src={recipe.image} alt={recipe.title} />}
       {recipe.title && <h3>{recipe.title}</h3>}
-      {recipe.sourceName && <p className="author">By {recipe.sourceName}</p>}
     </Link>
   );
 };
