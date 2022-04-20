e010f5f5ff84bb0458a980d8208eaa600dc2407b
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-04-20 11:50:32 -0400

    implement functionality for different recipe type page and style it

---
+++ src/pages/Type.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import RecipePreview from "../components/RecipePreview";
+import "./Type.scss";
+
+const Type = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  let params = useParams();
+
+  useEffect(() => {
+    getRecipes(params.type);
+  }, [params.type]);
+
+  const getRecipes = async (type) => {
+    const checkType = localStorage.getItem(`${params.type}`);
+
+    const URL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&type=${type}&number=9`;
+
+    if (checkType) {
+      setRecipes(JSON.parse(checkType));
+    } else {
+      await axios
+        .get(URL)
+        .then((res) => {
+          localStorage.setItem(`${type}`, JSON.stringify(res.data.results));
+          setRecipes(res.data.results);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  };
+
+  const recipeType = recipes.map((recipe) => {
+    return <RecipePreview key={recipe.id} recipe={recipe} />;
+  });
+
+  return <div className="recipe-type">{recipeType}</div>;
+};
+
+export default Type;
