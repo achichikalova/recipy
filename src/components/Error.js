da23cb23061d36109e5b9a225013eae15205fcdf
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-04-27 16:07:26 -0400

    add style to error component and change the message

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,12 +1,13 @@
 import React from "react";
+import "./Error.scss";
 
 const Error = ({ error }) => {
   return (
-    <div>
+    <div className="error">
       {error && (
         <h1>
-          "Sorry, it looks like you've reached the request limit. Please, could
-          you return tomorrow?"
+          "Sorry, it looks like you've reached the free request limit. <br></br>{" "}
+          Could you return tomorrow, please?"
         </h1>
       )}
     </div>
